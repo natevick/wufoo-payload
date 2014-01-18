d3c268c699847e546aa188ca7435f44816507232
Nate Vick <nvick@newheights.org>
2014-01-18 07:49:28 -0800

    Added parsed fields to results

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'json', '1.8.1'
+gem 'activesupport', '4.0.2'
 # Specify your gem's dependencies in wufoo-payload.gemspec
 gemspec
--- lib/wufoo/payload/parser.rb
+++ lib/wufoo/payload/parser.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/string'
+
 module Wufoo
 	module Payload
 
@@ -41,14 +43,6 @@ module Wufoo
         @fields.each { |field, response| @fields[field] = response[0] }
       end
       #@result is an object and should be a collection object for your payload.
-      def underscore
-        self.gsub(/::/, '/').
-          gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-          gsub(/([a-z\d])([A-Z])/,'\1_\2').
-          tr("-", "_").
-          downcase
-      end
-
 		end
 
 	end
