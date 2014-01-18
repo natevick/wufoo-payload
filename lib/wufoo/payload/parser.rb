774fd4bad890ad540784a6a94d75e66a809f4b01
Nate Vick <nvick@newheights.org>
2014-01-18 07:52:47 -0800

    Added parsed fields to results with the changes saved

--- lib/wufoo/payload/parser.rb
+++ lib/wufoo/payload/parser.rb
@@ -25,10 +25,11 @@ module Wufoo
         end
         @field_structure['Fields'].each do |field|
           field.each do |attribute, value|
-            if attribute == "Title" and value != nil
-              @v = FieldStructure.new
-            end
-            @v.send "#{attribute.underscore}=", value
+            @field = FieldStructure.new
+            @field.send "#{attribute.underscore}=", value
+            @field.title.each_with_object({}) {|title, join| join[title.squish.downcase.tr(" ", "_")], = @fields.values_at(@field.id)}
+            @field.each { |title, answer| @field[title] = answer[0] }
+            @result.merge(@field)
           end
         end
       end
@@ -41,6 +42,10 @@ module Wufoo
         field_keys = @data.keys.grep (/\AField\d/)
         @fields = field_keys.each_with_object({}) {|keys, responses| responses[keys] = @data.values_at(keys)}
         @fields.each { |field, response| @fields[field] = response[0] }
+      end
+
+      def joiner
+
       end
       #@result is an object and should be a collection object for your payload.
 		end
